96c061ef04f70fb167bde7fa19d325133336bf71
saka-guchi <sakaguchi0226@yahoo.co.jp>
2019-07-29 23:51:10 +0900

    Initial Commit

---
+++ algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by 坂口翔平 on 2019/07/29.
+//  Copyright © 2019 saka-guchi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
